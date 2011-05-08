c9352c7517e39843229b64204ef67306df211e16
Alberto Perdomo <alberto.perdomo@aentos.es>
2011-05-08 14:02:39 +0100

    Fixed broken acceptance test.

--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -1,12 +1,12 @@
 Then /^I should see the (\w+) top (\d+)$/ do |model, size|
   data = case model
-    when 'organizations' then { :selector => 'div.section.left', :title => 'Entidades' }
-    when 'projects' then { :selector => 'div.section.right', :title => 'Proyectos' }
+    when 'organizations' then { :selector => 'div.section.left', :title => 'Top entidades' }
+    when 'projects' then { :selector => 'div.section.right', :title => 'Top proyectos' }
     else raise Exception.new("unknown top")
   end
-  
+
   within(data[:selector]) do
     Then %{I should see "#{data[:title]}" in the title}
     page.should have_xpath('//ol/li', :count => size.to_i)
   end
-end               
+end
