9b0d2d60941a497858c5df0249d1f774306c408c
Alberto Perdomo <alberto.perdomo@aentos.es>
2011-05-08 20:45:32 +0100

    Fixing unit tests.

--- test/factories/organizations.rb
+++ test/factories/organizations.rb
@@ -1,6 +1,6 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :organization do |f|
-  f.name { Faker::Company.name }
+  f.name { SecureRandom.hex(10) }
   f.cif  { ((rand * 10e7).truncate.to_s.ljust(8, '0') + ("A".."Z").to_a[rand(("A".."Z").to_a.size)]) }
 end
