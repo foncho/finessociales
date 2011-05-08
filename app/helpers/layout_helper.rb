1f1664fa4ccf08819f203495ee1b6b0a439ca3b7
Juanjo Andres <juanjo.andres@aentos.es>
2011-05-08 22:14:00 +0100

    added link in menu

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -3,6 +3,7 @@ module LayoutHelper
   def render_navigation
     content_tag(:ul) do
       concat content_tag(:li, link_to('Inicio',root_path), :class => (current_page?(root_path)) ? 'selected first' : 'first')
+      concat content_tag(:li, link_to('Colectivos',groups_path), :class => (current_page?(groups_path)) ?  'selected' : nil)
       concat content_tag(:li, link_to('Organizaciones',organizations_path), :class => (current_page?(organizations_path)) ? 'selected' : nil)
       concat content_tag(:li, link_to('Sobre',root_path), :class => (current_page?(root_path)) ? 'selected' : nil)
     end
