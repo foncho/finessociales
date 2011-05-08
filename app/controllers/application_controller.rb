b82a41394079fbe4757d775563dadcd548781d53
Alberto Perdomo <alberto.perdomo@aentos.es>
2011-05-08 22:59:54 +0100

    Use cache headers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_cache_headers
+
   protected
   def ensure_current_url
     redirect_to resource, :status => :moved_permanently unless resource.friendly_id_status.best? || resource.nil?
+  end
+
+  def set_cache_headers
+    response.headers['Cache-Control'] = 'public, max-age=300'
   end
 end
